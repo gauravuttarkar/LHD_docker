9e4370fb28ecfff451f68fcc986d52ea3d2548dd
gauravuttarkar <gaurav.uttarkar@gmail.com>
2019-09-25 19:23:23 +0530

    Fixed some errors

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -1,7 +1,6 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from . import templates
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
